8ac90a3460038d00737ea93b8439f86f84c5b9cb
Ulyana Valodzina <67861703+Valodzina@users.noreply.github.com>
2020-09-29 12:27:19 +0300

    feat: compleate task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 
 exports.min = function min (array) {
+
+  if (array === null||array===undefined)
   return 0;
+  let res=0;
+  if (array.length >0) res= array[0];
+  for (let i =0; i<array.length; i++)
+  {
+    if ( array[i]<res) 
+    res= array[i];
+  }
+
+  return res;
 }
 
 exports.max = function max (array) {
+  if (array === null||array===undefined)
   return 0;
+  let res=0;
+  if (array.length >0) res= array[0];
+  for (let i =0; i<array.length; i++)
+  {
+    if ( array[i]>res) 
+    res= array[i];
+  }
+
+  return res;
 }
 
 exports.avg = function avg (array) {
+  if (array === null||array===undefined)
   return 0;
+  let res=0;
+
+  for (let i =0; i<array.length; i++)
+  {
+    res += array[i];
+  }
+
+
+if (array.length!=0)
+{
+res/=array.length;
+return res;
+}
+else return 0;
+
+
 }
